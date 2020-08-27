838acd2cfecdc3b603f34fb901c3800fccb75e5f
Chence5 <chence5@oocl.com>
2020-08-27 21:10:46 +0800

    [homework-refactor][cendeal]refactor:add nyc to show the coverage

--- src/rank.js
+++ src/rank.js
@@ -72,27 +72,6 @@ function rating(voyage, history) {
     }
 }
 
-module.exports = {};
-
-const voyage = {
-    zone: 'west-indies',
-    length: 10,
+module.exports = {
+    rating
 };
-const history = [
-    {
-        zone: 'east-indies',
-        profit: 5,
-    }, {
-        zone: 'west-indies',
-        profit: 15,
-    }, {
-        zone: 'china',
-        profit: -2,
-    },
-    {
-        zone: 'west-africa',
-        profit: 7,
-    },
-];
-const myRating = rating(voyage, history);
-console.log(`myRating: ${myRating}`);
--- test/rankTest.js
+++ test/rankTest.js
@@ -1,9 +1,30 @@
 const rankTest = require('ava');
-
+const {rannk} = require('../src/rank')
 rankTest('foo', t => {
   t.pass();
 });
-
+// const voyage = {
+//   zone: 'west-indies',
+//   length: 10,
+// };
+// const history = [
+//   {
+//     zone: 'east-indies',
+//     profit: 5,
+//   }, {
+//     zone: 'west-indies',
+//     profit: 15,
+//   }, {
+//     zone: 'china',
+//     profit: -2,
+//   },
+//   {
+//     zone: 'west-africa',
+//     profit: 7,
+//   },
+// ];
+// const myRating = rating(voyage, history);
+// console.log(`myRating: ${myRating}`);
 rankTest('bar', async t => {
   const bar = Promise.resolve('bar');
   t.is(await bar, 'bar');
