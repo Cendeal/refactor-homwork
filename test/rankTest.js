94f8bb60b4140bd3a130288afd90afa3129d02d6
Chence5 <chence5@oocl.com>
2020-08-27 21:30:18 +0800

    [homework-refactor][cendeal]test:add "should return B when rating given voyage zone is west-indies and history with china" test

--- test/rankTest.js
+++ test/rankTest.js
@@ -1,31 +1,30 @@
 const rankTest = require('ava');
-const {rannk} = require('../src/rank')
-rankTest('foo', t => {
-  t.pass();
-});
-// const voyage = {
-//   zone: 'west-indies',
-//   length: 10,
-// };
-// const history = [
-//   {
-//     zone: 'east-indies',
-//     profit: 5,
-//   }, {
-//     zone: 'west-indies',
-//     profit: 15,
-//   }, {
-//     zone: 'china',
-//     profit: -2,
-//   },
-//   {
-//     zone: 'west-africa',
-//     profit: 7,
-//   },
-// ];
-// const myRating = rating(voyage, history);
-// console.log(`myRating: ${myRating}`);
-rankTest('bar', async t => {
-  const bar = Promise.resolve('bar');
-  t.is(await bar, 'bar');
+const {rating} = require('../src/rank')
+
+rankTest('should return B when rating given voyage zone is west-indies and history with china', t => {
+    //given
+    const voyage = {
+        zone: 'west-indies',
+        length: 10,
+    };
+    const history = [
+        {
+            zone: 'east-indies',
+            profit: 5,
+        }, {
+            zone: 'west-indies',
+            profit: 15,
+        }, {
+            zone: 'china',
+            profit: -2,
+        },
+        {
+            zone: 'west-africa',
+            profit: 7,
+        },
+    ];
+    //when
+    const result = rating(voyage, history)
+    //then
+    t.is('B',result)
 });
