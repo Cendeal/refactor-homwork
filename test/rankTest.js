74c14cc12a8db282cce91cd1c44932d1c0248d42
Chence5 <chence5@oocl.com>
2020-08-28 01:09:19 +0800

    [homework-refactor][cendeal]test:add "should return A when rating given voyage zone is east-indies and history with china" test

--- test/rankTest.js
+++ test/rankTest.js
@@ -56,3 +56,31 @@ rankTest('should return A when rating given voyage zone is china and history wit
     //then
     t.is('A', result)
 });
+
+rankTest('should return A when rating given voyage zone is east-indies and history with china', t => {
+    //given
+    const voyage = {
+        zone: 'east-indies',
+        length: 10,
+    };
+    const history = [
+        {
+            zone: 'east-indies',
+            profit: 5,
+        }, {
+            zone: 'west-indies',
+            profit: 15,
+        }, {
+            zone: 'china',
+            profit: -2,
+        },
+        {
+            zone: 'west-africa',
+            profit: 7,
+        },
+    ];
+    //when
+    const result = rating(voyage, history)
+    //then
+    t.is('B', result)
+});
