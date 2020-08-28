a3752799966296bf4c8ff3d34ab5780b6962f62a
Chence5 <chence5@oocl.com>
2020-08-28 19:46:06 +0800

    [homework-refactor][cendeal]refactor: refactor delivery.js

--- src/delivery.js
+++ src/delivery.js
@@ -1,15 +1,38 @@
+function isMAOrCT(anOrder) {
+    return [
+        'MA',
+        'CT',
+    ].includes(anOrder.deliveryState);
+}
+
+function isNYorNH(anOrder) {
+    return [
+        'NY',
+        'NH',
+    ].includes(anOrder.deliveryState);
+}
+
+function isMAOrCTOrNY(anOrder) {
+    return [
+        'MA',
+        'CT',
+        'NY',
+    ].includes(anOrder.deliveryState);
+}
+
+function isMEOrNH(anOrder) {
+    return [
+        'ME',
+        'NH',
+    ].includes(anOrder.deliveryState);
+}
+
 function deliveryDate(anOrder, isRush) {
     if (isRush) {
         let deliveryTime;
-        if ([
-            'MA',
-            'CT',
-        ].includes(anOrder.deliveryState)) {
+        if (isMAOrCT(anOrder)) {
             deliveryTime = 1;
-        } else if ([
-            'NY',
-            'NH',
-        ].includes(anOrder.deliveryState)) {
+        } else if (isNYorNH(anOrder)) {
             deliveryTime = 2;
         } else {
             deliveryTime = 3;
@@ -17,20 +40,17 @@ function deliveryDate(anOrder, isRush) {
         return anOrder.placedOn.plusDays(1 + deliveryTime);
     } else {
         let deliveryTime;
-        if ([
-            'MA',
-            'CT',
-            'NY',
-        ].includes(anOrder.deliveryState)) {
+        if (isMAOrCTOrNY(anOrder)) {
             deliveryTime = 2;
-        } else if ([
-            'ME',
-            'NH',
-        ].includes(anOrder.deliveryState)) {
+        } else if (isMEOrNH(anOrder)) {
             deliveryTime = 3;
         } else {
             deliveryTime = 4;
         }
         return anOrder.placedOn.plusDays(2 + deliveryTime);
     }
+}
+
+module.exports = {
+    deliveryDate
 }
